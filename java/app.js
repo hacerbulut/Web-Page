d5b6f71f6f4c733dfcd9172c566f37f6dacb88f8
hacer <hcrleather@gmail.com>
2022-12-13 13:11:33 +0300

    gngn

---
+++ java/app.js
@@ -0,0 +1,27 @@
+
+
+
+// const o = {
+//     ad:"hacer",
+//     soyAd: "bulut",
+//     yas: 41,
+//     meslek:"sanatci",
+// }
+
+
+
+// console.log(typeof o);
+
+//  console.log(o);
+// console.log(5>3);
+// console.log(5<=3);
+// console.log(5<3);
+// console.log(5!=3);
+// console.log(5===3);
+
+console.log(5>3) &&(8 >= 6) && (4>2);
+console.log((5>1) || (4 >9));
+
+
+
+
