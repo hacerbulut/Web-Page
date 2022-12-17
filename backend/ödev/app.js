204ac4057f9683cf5d52b27e2046918b569c53c1
hacer <hcrleather@gmail.com>
2022-12-17 15:54:09 +0300

    google page

---
+++ backend/ödev/app.js
@@ -0,0 +1,73 @@
+// ÖDEV 1
+
+// const number1 = Number(prompt("enter first number"))
+// const process = prompt("please write transaction")
+// const number2 = Number(prompt("enter second number"))
+
+// if (process == "+"){
+//     console.log(number1 + number2);
+// }
+// else if (process =="-"){
+//     console.log(number1 - number2);
+// }
+// else if (process=="*"){
+//     console.log(number1 * number2);
+// }
+// else if (process =="/"){
+//     console.log(number1 / number2);
+// }
+// else{
+// console.log("hatali giriş yaptiniz");
+// }
+
+// ÖDEV 2
+
+// let day = prompt("please write the date")
+// switch (day) {
+//     case "Monday":
+//     case "Tuesday":
+//     case "Wednesday":
+//     case "Thursday":
+//         console.log("in-class");
+//         break;
+//     case "Friday":
+//     console.log("Teamwork");
+//         break;
+//     case "Saturday":
+//         console.log("inclass"+"workshop");
+//         break;
+//     case "Sunday":
+//         console.log("Self-study");
+//         break;
+//         default:
+//             alert("You entered incorrectly");
+//             break
+// }
+
+
+// ÖDEV 3
+
+
+// let salary = Number(prompt("enter your salary"));
+
+// const  minumum = 5500;
+// if (salary <= minumum){
+//    let newSalary = salary *1.5
+//    console.log(newSalary);
+// }else if(salary > 5500){
+//     let newSalary = Math.trunc(salary *1.1);
+//     console.log(newSalary);
+// }
+
+
+// ÖDEV 4
+
+
+// const salary = +prompt("please enter your income");
+// const expence = +prompt("please enter your expence");
+// const minumumWage = 5500;
+// let result=(salary - expence) > minumumWage
+// ? "Loan available" : "Loan not available";
+// console.log(result);
+// alert(result)
+
